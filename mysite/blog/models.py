17aabc86ddfc4c0fb9e7f3a278dc6036ed5255dd
Gonataev Ruslan <ruslan_98.6@mail.ru>
2020-04-08 02:35:43 +0300

    fix mistakes

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -36,7 +36,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
-                                                 self.publish.strftime('%m'),
-                                                 self.publish.strftime('%d'),
+                                                 self.publish.month,
+                                                 self.publish.day,
                                                  self.slug])
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include(('blog.urls', 'blog'), namespace='blog')),
+    path('admin/', admin.site.urls),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
 ]
